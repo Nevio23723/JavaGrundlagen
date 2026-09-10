f6d91dd9030caab03fe4206652c88755842276ae
nevio <nevio.weishaupt@ksb-sg.ch>
2026-09-10 17:37:25 +0200

    feat: finished the array inspector task

--- src/day_05_basics/ArrayInspector.java
+++ src/day_05_basics/ArrayInspector.java
@@ -2,7 +2,15 @@ package day_05_basics;
 
 public class ArrayInspector {
     public static void main(String[] args) throws Exception {
-        
+        int[] nums = {4, 25, 7, 19, 35, 8};
+        int max = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+        System.out.println("Die grösste Zahl ist: " + max);
     }
 }
 
