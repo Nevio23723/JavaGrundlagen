93113ae160ed23ecafd4b478904b2174096c2eac
Nevio.Weishaupt <nevio.weishaupt@ksb-sg.ch>
2026-09-11 08:34:13 +0200

    feat: Added an email to the user which is already validated. Email cannot be blank and has to be formatted right.

--- src/GymSim/Main.java
+++ src/GymSim/Main.java
@@ -5,12 +5,15 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) throws Exception {
         try {
-        User user = new User("Weishaupt ", "Nevio", LocalDate.of(2008, 07, 23));
+        User user = new User("Weishaupt ", "Nevio", LocalDate.of(2008, 07, 23),"nevio.weishaupt@gmail.com");
         System.out.println("User erfolgreich erstellt: " + user.getUsername());
         } catch (ValidationException e) {
             System.out.println("Fehler beim Erstellen des Benutzers: " + e.getMessage());
         }
-        
+        finally {
+            // spätere features 
+            // databaseConnection.close();
+        }
 
         
         
--- src/GymSim/User.java
+++ src/GymSim/User.java
@@ -1,22 +1,29 @@
 package gymsim;
 
 import java.time.LocalDate;
-
+import java.util.regex.Pattern;
 
 
 public class User {
     String name;
     String vorname;
     LocalDate geburtstag;
+    String email;
     
 
     
-    public User(String name, String vorname, LocalDate geburtstag) {
+    public User(String name, String vorname, LocalDate geburtstag, String email) {
         if (name.isBlank()) {
             throw new ValidationException("Name darf nicht leer sein.");
         }
         if (vorname.isBlank()) {
             throw new ValidationException("Vorname darf nicht leer sein.");
+        }
+        if (email.isBlank()) {
+            throw new ValidationException("Email darf nicht leer sein.");
+        }
+        if (!EMAIL_PATTERN.matcher(email).matches()) {
+            throw new ValidationException("Ungültiges Mail-Format:");
         }
         if (geburtstag.isAfter(LocalDate.now())) {
             throw new ValidationException("Geburtstag darf nicht später wie heute sein.");
@@ -31,8 +38,7 @@ public class User {
         this.name = name;
         this.vorname = vorname;
         this.geburtstag = geburtstag;
-        
-       
+        this.email = email.trim().toLowerCase();
     }
 
     public String toString() {
@@ -42,6 +48,11 @@ public class User {
     public String getUsername() {
         return vorname + " " + name;
     }
+
+    private static final Pattern EMAIL_PATTERN =
+        Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+
+
 
     
 
